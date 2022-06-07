afd977dfb7fba23a08eb654e12ba3b4801988d99
Amin Rezgui <amin.rezgui@softt365.com>
2022-06-07 23:05:56 +0100

    test 2

---
+++ src/calculService.js
@@ -0,0 +1,10 @@
+const calculService = {
+  sum_variation: (v1, v2) => {
+    return v1 + v2;
+  },
+  percent: (a, n) => {
+    return (a * 100) / n + " %";
+  },
+};
+
+export default calculService;
